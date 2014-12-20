7c1f1ed0111e42d21eed584c77671dc56d500593
cdroulers <droulers.christian@gmail.com>
2014-12-20 09:53:11 -0500

    Chapter 1. Form and list of products

--- koCart/js/viewmodel.js
+++ koCart/js/viewmodel.js
@@ -1,10 +1,40 @@
 var vm = (function () {
     "use strict";
 
-    var product = new Product(1,"T-Shirt", 10.00, 20);
+    var catalog = ko.observableArray([
+        new Product(1, "T-Shirt", 10.00, 20),
+        new Product(2, "Trousers", 20.00, 10),
+        new Product(3, "Shirt", 15.00, 20),
+        new Product(4, "Shorts", 5.00, 10)
+    ]);
+
+    var newProduct = {
+        name:ko.observable(''),
+        price:ko.observable(''),
+        stock:ko.observable(''),
+        clear: function() {
+            this.name('');
+            this.price('');
+            this.stock('');
+        }
+    };
+
+    var addProduct = function (context) {
+        var id = new Date().valueOf();//random id from time
+        var product = new Product(
+            id,
+            context.name(),
+            context.price(),
+            context.stock()
+            );
+        catalog.push(product);
+        newProduct.clear();
+    };
 
     return {
-        product: product
+        catalog: catalog,
+        newProduct: newProduct,
+        addProduct: addProduct
     };
 })();
 
